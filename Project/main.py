ab73ee98e3f71b6d77a945dd54cd61550ebe0395
AshrafElshazly <ashrafshazli7@gmail.com>
2022-05-24 07:20:37 +0200

    v1.1

--- Project/main.py
+++ Project/main.py
@@ -2,6 +2,9 @@ import model
 from os.path import exists
 import joblib
 import dreamer
+import ecg as PFecg
+import numpy as np
+import pandas as pd
 
 
 def run(train_path: str, dreamer_path: str):
@@ -18,4 +21,28 @@ def run(train_path: str, dreamer_path: str):
     return result
 
 
-# print(run("Data/dataML.csv", "../Datasets/DREAMER.mat"))
+def app(**kwargs):
+    test_path = kwargs.get('test', "")
+    ecg_path = kwargs.get('ecg', "")
+    heartrate = kwargs.get('heartrate', 80)
+
+    emotions = ['calm', 'happy', 'sad', 'angry', 'fear', 'suprise']
+    if(not exists('model.pkl')):
+        print(run("Data/dataML.csv", "../Datasets/DREAMER.mat"))
+
+    model = joblib.load('model.pkl')
+    if(exists(test_path)):
+        path = test_path
+    elif(exists(ecg_path)):
+        path = PFecg.PF_ecg(ecg_path)
+    else:
+        path = PFecg.PF_generated_ecg(heartrate)
+
+    data = pd.read_csv(path)
+    features = np.array(data.loc[:, 'ECG_Rate_Mean':'HRV_SampEn'])
+    features = np.nan_to_num(features)
+    emotion = model.predict(features)
+    return emotions[emotion[0]]
+
+
+print(app(test="data/emotions_signals/angry.csv"))
